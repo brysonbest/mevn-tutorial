b1293d9610b36be49dfc177013b27338edd09063
brysonbest <brycekalden@gmail.com>
2022-04-10 16:32:22 -0700

    Edited routing to address file creation issues.

--- nodejs-express-sequelize-mysql/app/controllers/tutorial.controller.js
+++ nodejs-express-sequelize-mysql/app/controllers/tutorial.controller.js
@@ -4,67 +4,67 @@ const Tag = db.tag;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Tutorial
-exports.create = (tutorial) => {
-  return Tutorial.create({
-    title: tutorial.title,
-    description: tutorial.description,
-  })
-    .then((tutorial) => {
-      console.log(">> Created Tutorial: " + JSON.stringify(tutorial, null, 4));
-      return tutorial;
-    })
-    .catch((err) => {
-      console.log(">> Error while creating Tutorial: ", err);
-    });
-};
-// Retrieve all Tutorials from the database.
-exports.findAll = () => {
-  return Tutorial.findAll({
-    include: [
-      {
-        model: Tag,
-        as: "tags",
-        attributes: ["id", "name"],
-        through: {
-          attributes: [],
-        },
-        // through: {
-        //   attributes: ["tag_id", "tutorial_id"],
-        // },
-      },
-    ],
-  })
-    .then((tutorials) => {
-      return tutorials;
-    })
-    .catch((err) => {
-      console.log(">> Error while retrieving Tutorials: ", err);
-    });
-};
-// Find a single Tutorial with an id
-exports.findById = (id) => {
-  return Tutorial.findByPk(id, {
-    include: [
-      {
-        model: Tag,
-        as: "tags",
-        attributes: ["id", "name"],
-        through: {
-          attributes: [],
-        },
-        // through: {
-        //   attributes: ["tag_id", "tutorial_id"],
-        // },
-      },
-    ],
-  })
-    .then((tutorial) => {
-      return tutorial;
-    })
-    .catch((err) => {
-      console.log(">> Error while finding Tutorial: ", err);
-    });
-};
+// exports.create = (tutorial) => {
+//   return Tutorial.create({
+//     title: tutorial.title,
+//     description: tutorial.description,
+//   })
+//     .then((tutorial) => {
+//       console.log(">> Created Tutorial: " + JSON.stringify(tutorial, null, 4));
+//       return tutorial;
+//     })
+//     .catch((err) => {
+//       console.log(">> Error while creating Tutorial: ", err);
+//     });
+// };
+// // Retrieve all Tutorials from the database.
+// exports.findAll = () => {
+//   return Tutorial.findAll({
+//     include: [
+//       {
+//         model: Tag,
+//         as: "tags",
+//         attributes: ["id", "name"],
+//         through: {
+//           attributes: [],
+//         },
+//         // through: {
+//         //   attributes: ["tag_id", "tutorial_id"],
+//         // },
+//       },
+//     ],
+//   })
+//     .then((tutorials) => {
+//       return tutorials;
+//     })
+//     .catch((err) => {
+//       console.log(">> Error while retrieving Tutorials: ", err);
+//     });
+// };
+// // Find a single Tutorial with an id
+// exports.findById = (id) => {
+//   return Tutorial.findByPk(id, {
+//     include: [
+//       {
+//         model: Tag,
+//         as: "tags",
+//         attributes: ["id", "name"],
+//         through: {
+//           attributes: [],
+//         },
+//         // through: {
+//         //   attributes: ["tag_id", "tutorial_id"],
+//         // },
+//       },
+//     ],
+//   })
+//     .then((tutorial) => {
+//       return tutorial;
+//     })
+//     .catch((err) => {
+//       console.log(">> Error while finding Tutorial: ", err);
+//     });
+// };
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {};
 // Delete a Tutorial with the specified id in the request
@@ -73,3 +73,139 @@ exports.delete = (req, res) => {};
 exports.deleteAll = (req, res) => {};
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {};
+
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+  // Create a Tutorial
+  const tutorial = {
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false,
+  };
+  // Save Tutorial in the database
+  Tutorial.create(tutorial)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  Tutorial.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Tutorial.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Tutorial with id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id,
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Tutorial.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Tutorial.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id,
+      });
+    });
+};
+
+exports.deleteAll = (req, res) => {
+  Tutorial.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Tutorials were deleted successfully!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials.",
+      });
+    });
+};
+
+exports.findAllPublished = (req, res) => {
+  Tutorial.findAll({ where: { published: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
