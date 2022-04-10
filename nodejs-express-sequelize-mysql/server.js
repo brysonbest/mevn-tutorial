1fe1e8bca92469b8aa65f30bbb64059d19e2b9c0
brysonbest <brycekalden@gmail.com>
2022-04-10 13:19:13 -0700

    Install and configure express server.

---
+++ nodejs-express-sequelize-mysql/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+//parse application/json file requests
+app.use(bodyParser.json());
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//home route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the test application!" });
+});
+
+//set port and initalize server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
